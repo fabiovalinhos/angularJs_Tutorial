ac3dca0d288521db1705856179bb787a64a9cb08
fabiovalinhos <fabiovalinhos@gmail.com>
2019-06-30 16:54:30 -0300

    Little adjustments

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,8 @@ angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
 
         // faz com que o angular deixa de trabalhar com # nas rotas e trabalha com html5, mas o backend tem que estar preparado
+        // tenho que colocar isto também no index.html
+        // <base href="/">
         $locationProvider.html5Mode(true);
         
         $routeProvider.when('/fotos', {
