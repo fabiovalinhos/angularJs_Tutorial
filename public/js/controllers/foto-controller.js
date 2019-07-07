9634c6cce77103c46df35be6c92f09f576447bab
fabiovalinhos <fabiovalinhos@gmail.com>
2019-07-07 18:42:34 -0300

    Finish upload feature

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,23 @@
-angular.module('alurapic').controller('FotoController', function($scope) {
+angular.module('alurapic').controller('FotoController', function($scope, $http) {
 
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function() {
-        console.log($scope.foto);
+        
+        // Pergunta para o formulário se tudo está ok e validado, ai sim grava no banco de dados
+        if ($scope.formulario.$valid) {
+            $http.post('v1/fotos', $scope.foto)
+            .success(function(){
+    
+                // após cadastrar a foto ele limpa o formulário
+                $scope.foto = {};
+                $scope.mensagem = "Foto cadastrada com sucesso";
+            })
+            .error(function(error){
+                $scope.mensagem = "Foto não cadastrada";
+                console.log(error);
+            });
+        }
     };
 });
