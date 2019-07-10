e14e667d94fe27721b3ab61890a12759c07610a1
fabiovalinhos <fabiovalinhos@gmail.com>
2019-07-09 22:03:32 -0300

    Editing and eraser photos

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,23 +1,51 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
+
+    // $routeParams me dá as informacoes dos parâmetros passados na rota
 
     $scope.foto = {};
     $scope.mensagem = '';
+
+    if ($routeParams.fotoId) {
+        $http.get('v1/fotos/' + $routeParams.fotoId)
+        .success(function(retorno) {
+            $scope.foto = retorno;
+        })
+        .error(function(erro){
+            console.log(erro);
+            $scope.mensagem = "Não foi possível obter a foto";
+        });
+    };
 
     $scope.submeter = function() {
         
         // Pergunta para o formulário se tudo está ok e validado, ai sim grava no banco de dados
         if ($scope.formulario.$valid) {
-            $http.post('v1/fotos', $scope.foto)
-            .success(function(){
-    
-                // após cadastrar a foto ele limpa o formulário
-                $scope.foto = {};
-                $scope.mensagem = "Foto cadastrada com sucesso";
-            })
-            .error(function(error){
-                $scope.mensagem = "Foto não cadastrada";
-                console.log(error);
-            });
+            
+            // se minha foto já tem um id (no caso uma edição) ... senão é uma foto nova
+            if ($scope.foto._id) {
+                $http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+                .success(function() {
+                    $scope.mensagem = "Atualização com sucesso da foto " + $scope.foto.titulo;
+                })
+                .error(function(erro){
+                    console.log(erro);
+                    $scope.mensagem = "Foto não foi atualizada";
+                });
+            }else{
+
+                $http.post('v1/fotos', $scope.foto)
+                .success(function(){
+        
+                    // após cadastrar a foto ele limpa o formulário
+                    $scope.foto = {};
+                    $scope.mensagem = "Foto cadastrada com sucesso";
+                })
+                .error(function(error){
+                    $scope.mensagem = "Foto não cadastrada";
+                    console.log(error);
+                });
+            }
+
         }
     };
 });
--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -3,8 +3,9 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	$scope.fotos = [];
 	// onde fica guardado o valor do meu ng-model que vem de principal.html
 	$scope.filtro = '';
+	$scope.mensagem = '';
 
-	$http.get('/v1/fotos')
+	$http.get('v1/fotos')
 	.success(function(retorno) {
 		console.log(retorno);
 		$scope.fotos = retorno; // não precisa fazer retorno.data
@@ -12,5 +13,22 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 	.error(function(erro) {
 		console.log(erro);
 	});
+
+	$scope.remover = function(foto) {
+		$http.delete('v1/fotos/' + foto._id)
+		.success(function() {
+
+			// retirando a foto da minha view ao invés de fazer uma nova querue no banco de dados
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+
+			$scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso';
+		})
+		.error(function(erro) {
+			console.log(erro);
+			$scope.mensagem = 'Não foi possível remover a foto ' + foto.titulo;
+		});
+
+	};
 
 });
